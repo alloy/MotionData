8a05a7d08b88044aad4105c9feb358ff35829ea0
Jonathan Penn <jonathan@navel-labs.com>
2012-05-25 08:23:56 -0400

    Comment out schema dump example so I can get specs passing

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -8,24 +8,24 @@ class AppDelegate
     #puts
 
     # Output from Schema.current.to_ruby
-    dumped_schema = Schema.define_version('1.0') do |s|
-      s.add_entity do |e|
-        e.name = 'Article'
-        e.managedObjectClassName = 'Article'
-        e.add_property :published, Boolean, {:default=>false}
-        e.add_property :title, String, {:required=>true}
-        e.add_property :body, String, {:required=>true}
-      end
+    # dumped_schema = Schema.define_version('1.0') do |s|
+    #   s.add_entity do |e|
+    #     e.name = 'Article'
+    #     e.managedObjectClassName = 'Article'
+    #     e.add_property :published, Boolean, {:default=>false}
+    #     e.add_property :title, String, {:required=>true}
+    #     e.add_property :body, String, {:required=>true}
+    #   end
 
-      s.add_entity do |e|
-        e.name = 'Author'
-        e.managedObjectClassName = 'Author'
-        e.add_property :name, String, {:required=>true}
-      end
-    end
+    #   s.add_entity do |e|
+    #     e.name = 'Author'
+    #     e.managedObjectClassName = 'Author'
+    #     e.add_property :name, String, {:required=>true}
+    #   end
+    # end
     # Dumping it again should look exactly the same.
     puts
-    puts dumped_schema.to_ruby
+    puts Schema.current.to_ruby
     puts
 
     true
